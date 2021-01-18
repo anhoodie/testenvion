02845e4f4edfc753b112f1df0d09cd3da04a1e74
anhoodie <110549531+anhoodie@users.noreply.github.com>
2021-01-18 19:42:08 +0000

    use strict mode

--- examples/simple.rs
+++ examples/simple.rs
@@ -7,4 +7,10 @@ use coinnect::poloniex::api::PoloniexApi;
 fn main() {
     // We create a PoloniexApi by providing API key/secret
     // You can give an empty String if you only use public methods
-    let mut my_api = PoloniexApi::new("api_key", "ap
+    let mut my_api = PoloniexApi::new("api_key", "api_secret");
+
+    // Let's look at the ticker!
+    let list_coins = my_api.return_ticker().unwrap();
+
+    for coin in list_coins {
+     
