d412dfe1a49871cfc1ff20b96fd5079a837d6249
anhoodie <110549531+anhoodie@users.noreply.github.com>
2021-01-18 16:26:40 +0000

    Improve spec

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,3 +1,8 @@
 // This example shows how to connect to your Poloniex account and perform simple operations
 
-extern crate
+extern crate coinnect;
+
+use coinnect::poloniex::api::PoloniexApi;
+
+fn main() {
+    // We create a PoloniexApi by providing AP
