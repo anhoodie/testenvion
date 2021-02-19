bae27ee9792d4d4cabe0657f4df9338aed730f33
anhoodie <110549531+anhoodie@users.noreply.github.com>
2021-02-19 19:24:06 +0000

    $message

--- src/error.rs
+++ src/error.rs
@@ -1,2 +1,5 @@
 //! This module contains enum Error.
-//! Error
+//! Error type represents all possible errors that can occur when dealing
+//! with the generic or any dedicated-exchange API
+
+use std::erro
