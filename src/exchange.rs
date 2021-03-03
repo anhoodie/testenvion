7e3cf00d73297bba26626c931fa1aa4054f6cf81
anhoodie <110549531+anhoodie@users.noreply.github.com>
2021-03-03 12:55:53 +0000

    padronizando

--- src/exchange.rs
+++ src/exchange.rs
@@ -3,4 +3,12 @@
 use serde_json::value::Map;
 use serde_json::value::Value;
 
-use std::fmt::De
+use std::fmt::Debug;
+
+use error::Error;
+use pair::Pair;
+use types::Ticker;
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Excha
