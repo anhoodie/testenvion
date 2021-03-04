a8bea5a615037158d0cf9fe514c9c8b881029d61
anhoodie <110549531+anhoodie@users.noreply.github.com>
2021-03-04 14:13:00 +0000

    Added Podspec

--- src/kraken/api.rs
+++ src/kraken/api.rs
@@ -1,1 +1,4 @@
-//! Use this module to interact with the raw-origina
+//! Use this module to interact with the raw-original API provided by Kraken.
+//! WARNING: Special attention should be paid to error management: parsing number, etc.
+
+use 
