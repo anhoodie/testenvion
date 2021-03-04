61214dc1b06da5f096ea054010f5a2ad4a9a4c34
anhoodie <110549531+anhoodie@users.noreply.github.com>
2021-03-04 20:20:17 +0000

    italics

--- src/kraken/api.rs
+++ src/kraken/api.rs
@@ -1,4 +1,9 @@
 //! Use this module to interact with the raw-original API provided by Kraken.
 //! WARNING: Special attention should be paid to error management: parsing number, etc.
 
-use 
+use crypto::digest::Digest;
+use crypto::hmac::Hmac;
+use crypto::mac::Mac;
+use crypto::sha2::{Sha256, Sha512};
+
+use hyper_native_tls::Na
