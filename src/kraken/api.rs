589f61933865210c4a0aa27ad71f135f2af24a8f
anhoodie <110549531+anhoodie@users.noreply.github.com>
2021-11-13 10:42:36 +0000

    Updated Indonesian translation

--- src/kraken/api.rs
+++ src/kraken/api.rs
@@ -578,4 +578,5 @@ impl KrakenApi {
     ///     no position = non-positional trades
     /// trades = whether or not to include trades related to position in output (optional.
     /// default = false)
-    /// 
+    /// start = starting unix timestamp or trade tx id of results (optional.  exclusive)
+    /// end = ending unix timestamp or trade
