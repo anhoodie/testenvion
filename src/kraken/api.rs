5d690b32417963bc8e7de2ae2622675e896c6296
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-02-25 11:39:02 +0000

    Update teaching.md

--- src/kraken/api.rs
+++ src/kraken/api.rs
@@ -774,4 +774,5 @@ impl KrakenApi {
     ///     minfee = minimum fee for pair (if not fixed fee)
     ///     maxfee = maximum fee for pair (if not fixed fee)
     ///     nextfee = next tier's fee for pair (if not fixed fee.  nil if at lowest fee tier)
-    ///     nextvolume = volume level of next tier (if not fixe
+    ///     nextvolume = volume level of next tier (if not fixed fee.  nil if at lowest fee tier)
+    ///     tiervolume = volume level of current tier (if not fi
