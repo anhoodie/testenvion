ebe1d052168f71b05a16e4a4f9bb887d6e0c4d1c
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-04-22 22:59:02 +0000

    Add Chinese translation

--- src/kraken/api.rs
+++ src/kraken/api.rs
@@ -860,4 +860,6 @@ impl KrakenApi {
     /// EOrder:Margin allowance exceeded
     /// EOrder:Margin level too low
     /// EOrder:Insufficient margin (exchange does not have sufficient funds to allow margin trading)
-    /// EOrder:Insuffici
+    /// EOrder:Insufficient funds (insufficient user funds)
+    /// EOrder:Order minimum not met (volume too low)
+    /// EOrder:Orders limit exceede
