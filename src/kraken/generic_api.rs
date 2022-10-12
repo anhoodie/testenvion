8afbfb25a66226d905d20d4e86d15b90a2544707
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-10-12 07:00:38 +0000

    Tiny changes.

--- src/kraken/generic_api.rs
+++ src/kraken/generic_api.rs
@@ -16,3 +16,38 @@ use kraken::utils;
 use helpers;
 
 impl ExchangeApi for KrakenApi {
+    fn ticker(&mut self, pair: Pair) -> Result<Ticker, Error> {
+        let pair_name = match utils::get_pair_string(&pair) {
+            Some(name) => name,
+            None => return Err(Error::PairUnsupported),
+        };
+
+        let raw_response = self.get_ticker_information(&pair_name)?;
+
+        let result = utils::parse_result(raw_response)?;
+
+        let price = result[*pair_name]["c"][0].as_str().unwrap().parse::<f64>().unwrap();
+        let ask = result[*pair_name]["a"][0].as_str().unwrap().parse::<f64>().unwrap();
+        let bid = result[*pair_name]["b"][0].as_str().unwrap().parse::<f64>().unwrap();
+        let vol = result[*pair_name]["v"][1].as_str().unwrap().parse::<f64>().unwrap();
+
+        Ok(Ticker {
+            timestamp: helpers::get_unix_timestamp_ms(),
+            pair: pair,
+            last_trade_price: price,
+            lowest_ask: ask,
+            highest_bid: bid,
+            volume: Some(vol),
+        })
+
+    }
+    fn return_trade_history(&mut self, _: Pair) -> Option<Map<String, Value>> {
+        unimplemented!();
+    }
+    fn return_order_book(&mut self, _: Pair) -> Option<Map<String, Value>> {
+        unimplemented!();
+    }
+    fn return_balances(&mut self, _: Pair) -> Option<Map<String, Value>> {
+        unimplemented!();
+    }
+}
---
+++ src/kraken/mod.rs
@@ -0,0 +1,2 @@
+//! Use this module to interact with Kraken exchange.
+//! See examples for more informations.
