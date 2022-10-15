f65ef811705db44f3a1405eddd76313569f20e56
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-10-15 11:57:58 +0000

    MIPS: Expose missing pci_io{map,unmap} declarations

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,10 @@
 //! All methods consume HTTPS api. The purpose of this crate is not
 //! to stream data (you should use websocket/FIX in that case).
 //!
-//! For optional parameters, most methods require an empty str (`""`) if yo
+//! For optional parameters, most methods require an empty str (`""`) if you don't want to specify
+//! them.
+//!
+//! ### Exchanges support:
+//! - [x] Poloniex
+//! - [x] Kraken
+//! - [x] Bitstamp (partial)
