022a8c7b52f0624ff0086227dae950e3012f9213
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-10-15 11:21:45 +0000

    new dashboard

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,6 @@
 //! Coinnect is a Rust library aiming to provide a complete access to REST APIs for various
 //! crypto-currencies exchanges (see below for a list of supported exchanges).
 //! All methods consume HTTPS api. The purpose of this crate is not
-//! to
+//! to stream data (you should use websocket/FIX in that case).
+//!
+//! For optional parameters, most methods require an empty str (`""`) if yo
