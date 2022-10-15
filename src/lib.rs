76838537c77d084fd703f0172b29edd477e1ba96
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-10-15 06:31:29 +0000

    Cleaned up documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
-//! ![Coinnect](https://raw.githubusercontent.com/hugues31/coinnect/master/co
+//! ![Coinnect](https://raw.githubusercontent.com/hugues31/coinnect/master/coinnect.png)
+//!
+//! Coinnect is a Rust library aiming to provide a complete access to REST APIs for various
+//! crypto-currencies exchang
