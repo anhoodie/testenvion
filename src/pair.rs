1b973f819ba4bcfb1d467446284a8ba3472c2724
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-10-30 22:19:03 +0000

    more tests and fixes

--- src/pair.rs
+++ src/pair.rs
@@ -1,3 +1,5 @@
 //! This module contains Pair enum.
 
-/// Pair lists all pairs that can be
+/// Pair lists all pairs that can be traded on supported exchanges.
+/// Update date : 11/04/2017.
+/// Note : Kraken uses XBT instead of BTC (so the XBT/E
