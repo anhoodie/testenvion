293360633dddb817100056099fdde7ffe78bbea9
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-11-05 16:22:05 +0000

    Major restructuring

--- src/pair.rs
+++ src/pair.rs
@@ -2,4 +2,5 @@
 
 /// Pair lists all pairs that can be traded on supported exchanges.
 /// Update date : 11/04/2017.
-/// Note : Kraken uses XBT instead of BTC (so the XBT/E
+/// Note : Kraken uses XBT instead of BTC (so the XBT/EUR pair becomes BTC/EUR).
+/// Order of quote currency <-> base currency is important. For exa
