668e30f84edc5562a313ce46f3564cd854fc1b79
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-11-11 15:04:54 +0000

    Add tables

--- src/pair.rs
+++ src/pair.rs
@@ -4,4 +4,10 @@
 /// Update date : 11/04/2017.
 /// Note : Kraken uses XBT instead of BTC (so the XBT/EUR pair becomes BTC/EUR).
 /// Order of quote currency <-> base currency is important. For example, Kraken supports ZEC_BTC
-/// but Poloniex is doing the opposite :  BTC_ZEC, which equal to 1/ZEC_BTC. So: ZEC_BTC != BTC_Z
+/// but Poloniex is doing the opposite :  BTC_ZEC, which equal to 1/ZEC_BTC. So: ZEC_BTC != BTC_ZEC
+#[derive(Debug, Copy, Clone, PartialEq)]
+#[allow(non_camel_case_types)]
+pub enum Pair {
+    BTC_AMP,
+    BTC_ARDR,
+ 
