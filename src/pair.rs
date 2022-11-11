92c24b078fa1a3dca2432d57290e9c2f152d3267
anhoodie <110549531+anhoodie@users.noreply.github.com>
2022-11-11 12:50:21 +0000

    f2fs: fix bugs and simplify codes of f2fs_fiemap

--- src/pair.rs
+++ src/pair.rs
@@ -3,4 +3,5 @@
 /// Pair lists all pairs that can be traded on supported exchanges.
 /// Update date : 11/04/2017.
 /// Note : Kraken uses XBT instead of BTC (so the XBT/EUR pair becomes BTC/EUR).
-/// Order of quote currency <-> base currency is important. For exa
+/// Order of quote currency <-> base currency is important. For example, Kraken supports ZEC_BTC
+/// but Poloniex is doing the opposite :  BTC_ZEC, which equal to 1/ZEC_BTC. So: ZEC_BTC != BTC_Z
