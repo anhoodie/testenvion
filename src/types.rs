66207ba12e65bdc7c63e885ecb59f17e30c84fb8
anhoodie <110549531+anhoodie@users.noreply.github.com>
2023-01-03 19:33:05 +0000

    printk: fix buffer overflow when calling log_prefix function from call_console_drivers

--- src/types.rs
+++ src/types.rs
@@ -7,4 +7,6 @@ type Volume = f64;
 
 #[derive(Debug)]
 pub struct Ticker {
-    /// UNIX timestamp in ms (when the response was recei
+    /// UNIX timestamp in ms (when the response was received)
+    pub timestamp: i64,
+    /// The Pair corresponding to the Ticker returned (maybe useful later for asynchronous APIs)
