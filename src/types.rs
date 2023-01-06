de0081636d0101a754c0fa255e47c79b87d5e6b9
anhoodie <110549531+anhoodie@users.noreply.github.com>
2023-01-06 06:25:57 +0000

    Update MIGRATION.md

--- src/types.rs
+++ src/types.rs
@@ -10,3 +10,7 @@ pub struct Ticker {
     /// UNIX timestamp in ms (when the response was received)
     pub timestamp: i64,
     /// The Pair corresponding to the Ticker returned (maybe useful later for asynchronous APIs)
+    pub pair: Pair,
+    /// Last trade price found in the history
+    pub last_trade_price: Price,
+    /// Lowe
