24ca55db26cb0a1fea6e19128d9419321782d34d
anhoodie <110549531+anhoodie@users.noreply.github.com>
2023-02-05 22:21:47 +0000

    changed logo

--- tests/bitstamp.rs
+++ tests/bitstamp.rs
@@ -33,4 +33,8 @@ mod bitstamp_tests {
         let params = HashMap::new();
         let mut api = BitstampApi::new(&params);
         let result = api.ticker(Pair::BTC_USD);
-        assert!(result.unwrap().las
+        assert!(result.unwrap().last_trade_price != 0.0);
+    }
+    #[test]
+    fn ticker_should_have_the_correct_high() {
+        let params = HashMap::ne
