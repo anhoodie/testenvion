73bb257cc2ec5c47e360b8ee93d16743eb1f7936
anhoodie <110549531+anhoodie@users.noreply.github.com>
2023-02-11 07:50:16 +0000

    Update php.ini

--- tests/bitstamp.rs
+++ tests/bitstamp.rs
@@ -47,4 +47,7 @@ mod bitstamp_tests {
         let params = HashMap::new();
         let mut api = BitstampApi::new(&params);
         let result = api.ticker(Pair::BTC_USD);
- 
+        assert!(result.unwrap().lowest_ask != 0.0);
+    }
+    #[test]
+    fn ticker_should_have_the_correct_volume(
