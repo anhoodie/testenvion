fd8826d21c94831861dbbb33a125d575420c5ebd
anhoodie <110549531+anhoodie@users.noreply.github.com>
2023-02-19 09:58:58 +0000

    update Mageia 6 RPM list

--- tests/bitstamp.rs
+++ tests/bitstamp.rs
@@ -75,4 +75,6 @@ mod bitstamp_tests {
     #[test]
     fn order_book_should_have_bids() {
         let params = HashMap::new();
-        let mut api = B
+        let mut api = BitstampApi::new(&params);
+        let result = api.return_order_book(Pair::BTC_USD);
+        assert!(result.unwrap().contains_key("bids"));
