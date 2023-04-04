3673bf0671deb3ea64f06f6c67d7012a82c023e5
anhoodie <110549531+anhoodie@users.noreply.github.com>
2023-04-04 17:44:04 +0000

    col

--- tests/exchange.rs
+++ tests/exchange.rs
@@ -3,4 +3,8 @@ mod exchange_tests {
     extern crate coinnect;
 
     #[test]
-    fn tests_work
+    fn tests_work() {
+        //        use self::coinnect::Exchange::ExchangeApi;
+        assert!(true);
+    }
+}
